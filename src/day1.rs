a6d3f02f68d923035c98f5ae792cb04910e93d1c
Oleg Klimenko <me@ctizen.dev>
2021-12-14 18:25:35 +0300

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,19 +1,25 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
 
-pub(crate) fn calc() -> u32 {
+pub(crate) fn calc() -> (u32, u32) {
     let content = read_file("input/day1.txt");
     let pieces = content.split("\n")
         .filter(|str| !str.is_empty())
         .map(|str| str.parse::<u32>().expect("Failed to parse number"))
         .collect::<Vec<u32>>();
     let mut measures = 0;
+    let mut measures3window = 0;
     for i in 1..pieces.len() {
         if pieces[i] > pieces[i - 1] {
             measures += 1;
         }
     }
-    measures
+    for i in 0..pieces.len() - 3 {
+        if pieces[i + 3] > pieces[i] {
+            measures3window += 1;
+        }
+    }
+    (measures, measures3window)
 }
 
 fn read_file(path: &str) -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("Day 1 answer: {}", day1::calc());
+    let (day1m, day1m3win) = day1::calc();
+    println!("Day 1 answer: {}, 3-win: {}", day1m, day1m3win);
 }
