e7f61db91aa2baa6f664b4f4d467cf8a032de7ef
Oleg Klimenko <me@ctizen.dev>
2021-12-24 22:21:02 +0300

    Day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -6,7 +6,7 @@ pub(crate) fn calc() -> u32 {
         .map(|str| String::from(str))
         .collect::<Vec<String>>();
     let row_size = content[0].len();
-    let nums = content.iter().flat_map(|str| str
+    let mut nums = content.iter().flat_map(|str| str
         .chars()
         .map(|char| match char {
             '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
@@ -14,18 +14,20 @@ pub(crate) fn calc() -> u32 {
         }).collect::<Vec<u8>>()
     ).collect::<Vec<u8>>();
 
-    println!("{}", nums.len());
-
     let mut mins = Vec::new();
     for i in 0..nums.len() {
         if is_min(&nums, row_size, i) {
-            mins.push(nums[i]);
+            mins.push(i);
         }
     }
 
-    println!("{}", mins.len());
+    let mut sizes = mins
+        .iter()
+        .map(|idx| find_basin(&mut nums, row_size, *idx))
+        .collect::<Vec<u32>>();
+    sizes.sort_by(|a, b| b.cmp(a));
 
-    mins.iter().map(|min| min + 1).fold(0u32, |acc, val| acc + val as u32)
+    sizes[0] * sizes[1] * sizes[2]
 }
 
 fn is_min(set: &Vec<u8>, row_size: usize, index: usize) -> bool {
@@ -48,4 +50,28 @@ fn is_min(set: &Vec<u8>, row_size: usize, index: usize) -> bool {
         is_min = is_min && set[index] < set[i4 as usize];
     }
     is_min
+}
+
+fn find_basin(set: &mut Vec<u8>, row_size: usize, index: usize) -> u32 {
+    let i1 = index as i32 - 1; // left
+    let i2 = index as i32 + 1; // right
+    let i3 = index as i32 + row_size as i32; // down
+    let i4 = index as i32 - row_size as i32; // up
+
+    let mut size: u32 = 1; // count current field
+    set[index] = 0;
+    if i1 > 0 && index as i32 % row_size as i32 != 0 && set[i1 as usize] > 0 && set[i1 as usize] < 9 {
+        size += find_basin(set, row_size, i1 as usize);
+    }
+    if i2 < set.len() as i32 && ((index as i32 + 1) % row_size as i32) != 0 && set[i2 as usize] > 0 && set[i2 as usize] < 9 {
+        size += find_basin(set, row_size, i2 as usize);
+    }
+    if i3 < set.len() as i32 && set[i3 as usize] > 0 && set[i3 as usize] < 9 {
+        size += find_basin(set, row_size, i3 as usize);
+    }
+    if i4 > 0 && set[i4 as usize] > 0 && set[i4 as usize] < 9 {
+        size += find_basin(set, row_size, i4 as usize);
+    }
+
+    size
 }
